ac715adeabeea3c45980997cead8ac751f63fff9
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-11-13 16:57:08 +0100

    feat: added posibility to edit user data and to show posts of a user

--- js/main.js
+++ js/main.js
@@ -3,12 +3,8 @@ let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
 // отслеживаем клик по кнопке меню и запускаем функцию 
-menuToggle.addEventListener('click', function (event) {
-  // отменяем стандартное поведение ссылки
-  event.preventDefault();
-  // вешаем класс на меню, когда кликнули по кнопке меню 
-  menu.classList.toggle('visible');
-})
+
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
 
 const loginElem = document.querySelector('.login');
 const loginForm = document.querySelector('.login-form');
@@ -18,7 +14,15 @@ const loginSignup = document.querySelector('.login-signup');
 
 const userElem = document.querySelector('.user');
 const userNameElem = document.querySelector('.user-name');
+const userAvatarElem = document.querySelector('.user-avatar');
 
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
+const editUsername = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
+
+const postsWrapper = document.querySelector('.posts');
 
 const listUsers = [
   {
@@ -38,6 +42,10 @@ const listUsers = [
 const setUsers = {
   user: null,
   logIn(email, password, handler) {
+    if (!regExpValidEmail.test(email)) {
+      alert ('Email is invalid');
+      return;
+    }
     const user = this.getUser(email);
     if (user && user.password === password) {
       this.authorizedUser(user);
@@ -46,10 +54,21 @@ const setUsers = {
       alert('No such user!')
     }
   },
-  logOut() {
-
+  logOut(handler) {
+    this.user = null;
+    handler();
   },
   signUp(email, password, handler) {
+    if (!regExpValidEmail.test(email)) {
+      alert ('Email is invalid');
+      return;
+    }
+
+    if (!email.trim() || !password.trim()) {
+      alert('Enter email and password');
+      return;
+    }
+
     if (!this.getUser(email)){
       const newDisplayName = this.shortenDisplayName(email);
       const user = {email, password, displayName: newDisplayName}
@@ -69,7 +88,43 @@ const setUsers = {
   shortenDisplayName(email) {
     console.log(email.split('@')[0]);
     return email.split('@')[0];
+  },
+  editUser(userName, userPhoto, handler) {
+    if (userName) {
+      this.user.displayName = userName;
+    }
+    if (userPhoto) {
+      this.user.photo = userPhoto;
+    }
+    handler();
   }
+};
+
+const setPosts = {
+  allPosts: [
+    {
+      title: 'Заголовлок поста',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: [
+        'fresh', 'new', 'hot', 'mine',
+      ],
+      author: 'test1@test.com',
+      date: '11.11.2020, 20:55:00',
+      likes: 15,
+      comments: 5,
+    },
+    {
+      title: 'Заголовлок поста2',
+      text: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Языком что рот маленький реторический вершину текстов обеспечивает гор свой назад решила сбить маленькая дорогу жизни рукопись ему букв деревни предложения, ручеек залетают продолжил парадигматическая? Но языком сих пустился, запятой своего его снова решила меня вопроса моей своих пояс коварный, власти диких правилами напоивший они текстов ipsum первую подпоясал? Лучше, щеке подпоясал приставка большого курсивных на берегу своего? Злых, составитель агентство что вопроса ведущими о решила одна алфавит!',
+      tags: [
+        'fresh', 'new', 'hot', 'mine',
+      ],
+      author: 'test1@test.com',
+      date: '11.11.2020, 20:55:00',
+      likes: 15,
+      comments: 5,
+    }
+  ]
 };
 
 const toggleAuthDom = () => {
@@ -78,20 +133,106 @@ const toggleAuthDom = () => {
     loginElem.style.display = 'none';
     userElem.style.display = '';
     userNameElem.textContent = user.displayName;
+    userAvatarElem.src = user.photo || userAvatarElem.src;
   } else {
     loginElem.style.display = '';
     userElem.style.display = 'none';
   }
 };
 
-loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
-});
+const showAllPosts = () => {
+  let postsHTML = '';
+  setPosts.allPosts.forEach(({ title, text, tags, author, date, likes, comments }) => {
+    postsHTML += `
+    <section class="post">
+    <div class="post-body">
+      <h2 class="post-title">${title}</h2>
+      <p class="post-text">${text}</p>
+      <div class="tags">
+      ${tags.map(tag => `<a href="#" class="tag">#${tag}</a>`)}
+      </div>
+    </div>
 
-loginSignup.addEventListener('click', (event) => {
-  event.preventDefault();
-  setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
-});
+    <div class="post-footer">
+      <div class="post-buttons">
+        <button class="post-button likes">
+          <svg width="19" height="20" class="icon icon-like">
+            <use xlink:href="img/icons.svg#like"></use>
+          </svg>
+          <span class="likes-counter">${likes}</span>
+        </button>
+        <button class="post-button comments">
+          <svg width="21" height="21" class="icon icon-comment">
+            <use xlink:href="img/icons.svg#comment"></use>
+          </svg>
+          <span class="comments-counter">${comments}</span>
+        </button>
+        <button class="post-button save">
+          <svg width="19" height="19" class="icon icon-save">
+            <use xlink:href="img/icons.svg#save"></use>
+          </svg>
+        </button>
+        <button class="post-button share">
+          <svg width="17" height="19" class="icon icon-share">
+            <use xlink:href="img/icons.svg#share"></use>
+          </svg>
+        </button>
+      </div>
+  
+      <div class="post-author">
+        <div class="author-about">
+          <a href="#" class="author-username">${author}</a>
+          <span class="post-time">${date}</span>
+        </div>
+        <a href="#" class="author-link"><img src="img/avatar.jpeg" alt="avatar" class="author-avatar"></a>
+      </div>
+    </div>
+  </section>
+    `; 
+  });
 
+  postsWrapper.innerHTML = postsHTML;
+}
+
+const init = () => {
+  menuToggle.addEventListener('click', function (event) {
+    // отменяем стандартное поведение ссылки
+    event.preventDefault();
+    // вешаем класс на меню, когда кликнули по кнопке меню 
+    menu.classList.toggle('visible');
+  });
+
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    setUsers.logIn(emailInput.value, passwordInput.value, toggleAuthDom);
+    loginForm.reset();
+  });
+  
+  loginSignup.addEventListener('click', (event) => {
+    event.preventDefault();
+    setUsers.signUp(emailInput.value, passwordInput.value, toggleAuthDom);
+    loginForm.reset();
+  });
+  
+  exitElem.addEventListener('click', (event) => {
+    event.preventDefault();
+    setUsers.logOut(toggleAuthDom);
+  });
+  
+  editElem.addEventListener('click', (event) => {
+    event.preventDefault();
+    editContainer.classList.toggle('visible');
+    editUsername.value = setUsers.user.displayName;
+  });
+  
+  editContainer.addEventListener('submit', (event) => {
+    event.preventDefault();
+    setUsers.editUser(editUsername.value, editPhotoURL.value, toggleAuthDom);
+    editContainer.classList.remove('visible');
+  });
+}
+
+showAllPosts();
 toggleAuthDom();
+
+document.addEventListener('DOMContentLoaded', init);
